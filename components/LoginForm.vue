11a143f5a4dab81abf356bd61f954f8d5466c401
close01 <sirinanp64@gmail.com>
2022-01-31 15:57:03 +0700

    login

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -17,6 +17,9 @@
             type="text"
             style="width: 270px; margin:15px auto 0px;"
           />
+          {{ profile }}
+          <!-- {{profile.u.indexof(input.username)}} -->
+          {{ typeof input.username }}
         </div>
         <div class="mb-4">
           <!-- <label class="block mb-2 text-gray-500" for="password">Password</label> -->
@@ -29,6 +32,8 @@
             type="password"
             style="width: 270px; margin:30px auto;"
           />
+          {{ input.password }}
+          <!-- {{this.$parent.mockAccount.username}} -->
         </div>
         <div>
           <a-button type="primary" style="width: 270px; height:40px" @click="login">
@@ -45,26 +50,47 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       input: {
         username: '',
         password: ''
-      }
+      },
+      profile: [],
+      profile1: []
     }
   },
+  mounted () {
+    this.getProfile()
+    this.getProfile1()
+  },
   methods: {
+    async getProfile () {
+      const res = await axios.get('http://localhost:3033/get/users')
+      this.profile = res.data
+      console.log('uuu', this.profile)
+      // console.log(this.input.username)
+    },
+    async getProfile1 () {
+      const res = await axios.get('http://localhost:3033/get/profiles/pw')
+      this.profile1 = res.data
+      console.log('ppp', this.profile1)
+      // console.log(this.input.username)
+    },
     login () {
       if (this.input.username !== '' && this.input.password !== '') {
-        if (this.input.username === this.$parent.mockAccount.username && this.input.password === this.$parent.mockAccount.password) {
-          this.$emit('authenticated', true)
-          this.$router.replace('/myprofile')
+        if (this.profile.includes(this.input.username) || this.profile1.includes(this.input.password)) {
+          if (this.input.username === this.profile.user && this.input.password === '') {
+            this.$emit('authenticated', true)
+            this.$router.replace('/myprofile')
+          } else {
+            console.log('The username and / or password is incorrect')
+          }
         } else {
-          console.log('The username and / or password is incorrect')
+          console.log('A username and password must be present')
         }
-      } else {
-        console.log('A username and password must be present')
       }
     }
   }
--- pages/myprofile.vue
+++ pages/myprofile.vue
@@ -2,8 +2,9 @@
   <div>
     <h1>Profile</h1>
     <MyProfile />
+    <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
     <a-button type="dashed" style="width: 270px; height:40px" @click="logout">
-      cancel
+      logout
     </a-button>
     <router-view @authenticated="setAuthenticated" />
   </div>
@@ -11,13 +12,14 @@
 
 <script>
 export default {
+  name: 'ProfilePage',
   data () {
     return {
-      authenticated: false,
-      mockAccount: {
-        username: 'aaaaaa',
-        password: 'aaaaaa'
-      }
+      authenticated: true
+      // mockAccount: {
+      //   username: 'aaaaaa',
+      //   password: 'aaaaaa'
+      // }
     }
   },
   mounted () {
@@ -28,9 +30,11 @@ export default {
   methods: {
     setAuthenticated (status) {
       this.authenticated = status
+      console.log('xzxzxzxz', status)
     },
     logout () {
       this.authenticated = false
+      this.$router.replace('/')
     }
   }
 }
